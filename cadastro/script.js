acb35b14e8be49b2c1fb26632dfcb60d7b3a0f54
joaoTRYHARD <joao.pietro.santos@escola.pr.gov.br>
2025-04-14 11:17:02 -0300

    cadastro_metas

---
+++ cadastro/script.js
@@ -0,0 +1,27 @@
+alert("Vamos realizar seu cadastro")
+
+var nome = prompt("insira seu nome");
+
+alert("você digitou seu nome");
+
+console.log(nome);
+
+var telefone = prompt("insira seu numero de telefone")
+
+alert("você digitou seu telefone")
+
+console.log(telefone)
+
+var email = prompt("digite seu email")
+
+alert("você digitou seu email")
+
+console.log(email)
+
+var senha = prompt("digite sua senha")
+
+alert("você digitou sua senha")
+
+console.log(senha)
+
+alert("seu cadastro foi concluido com sucesso")
